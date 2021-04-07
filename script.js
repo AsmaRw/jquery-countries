4e873427cfbb1729c71a719c582c31031550af8e
asma <asma.rw09@gmail.com>
2021-04-07 10:22:24 +0200

    jquery

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-$(document).ready(function () {
-    $('#btnShowCountries').click(function () {
-        function getCountries() {
-
-            $.ajax({
-                url: 'http://localhost:8000/countries',
-                success: function (data, statuts, response) {
-                    console.log(data);
-                }
-        })
-    }
+function getCountries() {
+    $.ajax({
+        url: "http://localhost:8000/countries",
+        success: function (data, status, response) {
+            for (var i = 0; i < data.length; i++) {
+                $("ul").append(`<li>${data.reverse()[i]}</li>`)
+            }
+        }
     })
+}
+
+$(function () {
+
+    $("#btnShowCountries").click(function () {
+
+        getCountries()
+
+    })
+
 })
 
